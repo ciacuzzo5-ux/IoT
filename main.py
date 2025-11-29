34809847c58d2f220e34e8d039457a1521ccd99b
ValeriaLupo <v.lupo7@studenti.unisa.it>
2025-11-29 19:15:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# questo è il main del progetto MUSEO di IOT che gestisce:
+# mqqt (pub/sub)
+#lettura dell'accelerometro
+# controllo led del quadro
+# allarme buzzer
+# gestione comandi ricevuti da nodered
+
+import time
+import machine
+from umqttsimple import MQTTClient
+from machine import Pin, I2C
+
+#import delle variabili definite in boot.py (ip broker
